0e3292e5b5852a680e9235f536ca258976c1eafc
J Shattu <jason.shattu@genesis.global>
2022-10-03 09:42:38 +0100

    feat: lockdown genesis, deployment plugin, kotlin and auth version and remove related questions from genx to reduce support issues with users entering in incorrect or incompatible values PTC-445

--- .genx/prompts/server.js
+++ .genx/prompts/server.js
@@ -1,43 +1,15 @@
 const {mavenArtifactVersionRegex} = require('./validators');
 
 module.exports = async (inquirer, prevAns = {}) => {
-    () => console.log(`Enter Genesis Server version`);
     const {
-      genesisVersion = prevAns.genesisVersion,
-      deployPluginVersion = prevAns.deployPluginVersion,
-      kotlinVersion = prevAns.kotlinVersion,
       groupId = prevAns.groupId,
       applicationVersion = prevAns.applicationVersion
     } = await inquirer.prompt([
       {
-        name: 'genesisVersion',
-        type: 'input',
-        message: 'Genesis Server version',
-        when: !prevAns.pkgScope,
-        default: '6.1.2',
-        validate: mavenArtifactVersionRegex,
-      },
-      {
-        name: 'deployPluginVersion',
-        type: 'input',
-        message: 'Genesis Deploy plugin version',
-        when: !prevAns.genesisVersion,
-        default:'6.1.2',
-        validate: mavenArtifactVersionRegex,
-      },
-      {
-        name: 'kotlinVersion',
-        type: 'input',
-        message: 'Kotlin version',
-        when: !prevAns.deployPluginVersion,
-        default:'1.6.10',
-        validate: mavenArtifactVersionRegex,
-      },
-      {
         name: 'groupId',
         type: 'input',
         message: 'Group Id',
-        when: !prevAns.kotlinVersion,
+        when: !prevAns.pkgScope,
         default:'global.genesis'
       },
       {
@@ -50,9 +22,6 @@ module.exports = async (inquirer, prevAns = {}) => {
       },
     ]);
     return {
-      genesisVersion,
-      deployPluginVersion,
-      kotlinVersion,
       groupId,
       applicationVersion
     };
