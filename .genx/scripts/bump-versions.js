7bac9fca58159cb8deb3b7d13641f3e33abb7b5c
Artur Cistov <artur.cistov@genesis.global>
2023-08-16 10:12:35 +0100

    chore: wip PSD-9

--- .genx/scripts/bump-versions.js
+++ .genx/scripts/bump-versions.js
@@ -2,7 +2,7 @@ const { execSync } = require('node:child_process');
 const { writeFileSync } = require('node:fs');
 const { resolve, dirname }  = require('node:path');
 
-const versions = require('../versions.json');
+const current = require('../versions.json');
 
 const run = (command) => execSync(command, {
     stdio: ['pipe', 'pipe', 'ignore'],
@@ -10,9 +10,11 @@ const run = (command) => execSync(command, {
     .toString('utf8')
     .trim();
 
-console.log('Current:', versions);
+console.log('Current:', current);
 
 const UI = run('npm info @genesislcap/foundation-ui@latest version');
 const GSF = run(`jf rt s "libs-release-client/global/genesis/genesis-distribution/" --sort-by=created --sort-order=desc --limit=1 --exclusions="*-RC*;*-SNAPSHOT*" | grep path | tr -s ' ' | cut -d '/' -f 5`);
 const Auth = run(`jf rt s "libs-release-client/global/genesis/auth-distribution/" --sort-by=created --sort-order=desc --limit=1 --exclusions="*-RC*;*-SNAPSHOT*" | grep path | tr -s ' ' | cut -d '/' -f 5`);
 console.log('Latest:', { UI, GSF, Auth });
+
+console.log('Diff', { UI: current.UI === UI, GSF: current.GSF === GSF, Auth: current.Auth === Auth });
