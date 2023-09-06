c5e0136fb3a81a7ff8e1aef5f93e0b0a4ddbcc90
Artur Cistov <artur.cistov@genesis.global>
2023-09-06 09:27:19 +0100

    chore: wip PSD-11

--- .genx/configure.js
+++ .genx/configure.js
@@ -40,34 +40,34 @@ module.exports = async (data, utils) => {
   [...filesClient, ...filesServer].forEach(item => writeFileWithData(item, data));
 
   // rename files & folders
-  const placeholder = "genesis";
-  const dictionaryConfigFolder = resolve(serverJvmRoot, "genesis-config", "src", "main", "resources", "cfg");
-  const scriptConfigFolder = resolve(serverJvmRoot, "genesis-script-config", "src", "main", "resources", "scripts");
-  const dictionaryCacheRoot = resolve(directory, "server", "jvm", "genesis-dictionary-cache");
-  [
-    { item: "genesis-fields-dictionary.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-tables-dictionary.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-view-dictionary.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-system-definition.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-processes.xml", dir: dictionaryConfigFolder },
-    { item: "genesis-service-definitions.xml", dir: dictionaryConfigFolder },
-    { item: "genesis-dataserver.kts", dir: scriptConfigFolder },
-    { item: "genesis-eventhandler.kts", dir: scriptConfigFolder },
-    { item: "genesis-reqrep.kts", dir: scriptConfigFolder },
-    { item: "genesis-generated-dao", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-fields", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-hft", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-sysdef", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-view", dir: dictionaryCacheRoot },
-    { item: "genesis-dictionary-cache", dir: serverJvmRoot },
-    { item: "genesis-config", dir: serverJvmRoot },
-    { item: "genesis-deploy", dir: serverJvmRoot },
-    { item: "genesis-distribution", dir: serverJvmRoot },
-    { item: "genesis-eventhandler", dir: serverJvmRoot },
-    { item: "genesis-messages", dir: serverJvmRoot },
-    { item: "genesis-script-config", dir: serverJvmRoot },
-    { item: "genesis-site-specific", dir: serverJvmRoot },
-  ].forEach(el => rename(el.item, el.item.replace(placeholder, appName), el.dir));
+  // const placeholder = "genesis";
+  // const dictionaryConfigFolder = resolve(serverJvmRoot, "genesis-config", "src", "main", "resources", "cfg");
+  // const scriptConfigFolder = resolve(serverJvmRoot, "genesis-script-config", "src", "main", "resources", "scripts");
+  // const dictionaryCacheRoot = resolve(directory, "server", "jvm", "genesis-dictionary-cache");
+  // [
+    // { item: "genesis-fields-dictionary.kts", dir: dictionaryConfigFolder },
+    // { item: "genesis-tables-dictionary.kts", dir: dictionaryConfigFolder },
+    // { item: "genesis-view-dictionary.kts", dir: dictionaryConfigFolder },
+    // { item: "genesis-system-definition.kts", dir: dictionaryConfigFolder },
+    // { item: "genesis-processes.xml", dir: dictionaryConfigFolder },
+    // { item: "genesis-service-definitions.xml", dir: dictionaryConfigFolder },
+    // { item: "genesis-dataserver.kts", dir: scriptConfigFolder },
+    // { item: "genesis-eventhandler.kts", dir: scriptConfigFolder },
+    // { item: "genesis-reqrep.kts", dir: scriptConfigFolder },
+    // { item: "genesis-generated-dao", dir: dictionaryCacheRoot },
+    // { item: "genesis-generated-fields", dir: dictionaryCacheRoot },
+    // { item: "genesis-generated-hft", dir: dictionaryCacheRoot },
+    // { item: "genesis-generated-sysdef", dir: dictionaryCacheRoot },
+    // { item: "genesis-generated-view", dir: dictionaryCacheRoot },
+    // { item: "genesis-dictionary-cache", dir: serverJvmRoot },
+    // { item: "genesis-config", dir: serverJvmRoot },
+    // { item: "genesis-deploy", dir: serverJvmRoot },
+    // { item: "genesis-distribution", dir: serverJvmRoot },
+    // { item: "genesis-eventhandler", dir: serverJvmRoot },
+    // { item: "genesis-messages", dir: serverJvmRoot },
+    // { item: "genesis-script-config", dir: serverJvmRoot },
+    // { item: "genesis-site-specific", dir: serverJvmRoot },
+  // ].forEach(el => rename(el.item, el.item.replace(placeholder, appName), el.dir));
 
   // persist answers
   writeJSON(`${__dirname}/answers.json`, data);
