a7b321583f6641eeaf95050ab8f0bbb43c25eac3
Artur Cistov <artur.cistov@genesis.global>
2023-09-06 14:54:02 +0100

    chore: wip PSD-11

--- .genx/.genxrc.js
+++ .genx/.genxrc.js
@@ -1,29 +1,20 @@
-const load = (path) => () => {
-  try {
-    return require(`./${path}`);
-  } catch (e) {
-    return {};
-  }
-}
-
 // GenX CLI Configuration
 module.exports = {
-  // Answers persisted during the latest configure call
-  answers: load('answers.json'),
-  // Build files and directories within the seed
-  build: load('build'),
   // Script used to configure the seed
   configure: load('configure'),
   // Additional seed details beyond what was captured during seed registration.
   details: load('details'),
-  // NPM scripts key map
-  npmScripts: load('npmScripts'),
-  // Lerna packages / directories within the seed.
-  packages: load('packages'),
   // Prompts used to capture seed configuration values.
   prompts: load('prompts'),
-  // Plop.js templates used for seed specific code generation.
-  templates: load('templates'),
-  // The tsconfig files within the seed.
-  tsconfigs: load('tsconfigs'),
+  options: {
+    persistAnswers: true,
+    processFiles: {
+      excludeDirs: ['.git', '.genx', '.github', 'node_modules', '.gradle', 'gradle'],
+      rename: true,
+      interpolateVariables: {
+        enabled: true,
+        excludeExts: ['', '.ico', '.png', '.jpg', '.gif', '.lock', '.jar', '.crt', '.key']
+      }
+    }
+  }
 };
--- .genx/prompts.js
+++ .genx/prompts.js
@@ -13,14 +13,6 @@ module.exports = async (inquirer, prevAns = {}) => {
   const {apiHost, enableSSO} = await apiPrompts(inquirer, prevAns)
   const {groupId, applicationVersion} = await genesisServerPrompts(inquirer, prevAns);
 
-  // data.pkgName = appName.replace(/[\W_]/g, '');
-  // data.rootElement = `${data.pkgName}-root`;
-  // data.localGenId = appName.toUpperCase().replace("-", "_");
-  // data.applicationVersionWeb = applicationVersion.split('-').shift();
-  // data.versions = versions;
-  // // to be exposed via user prompt in the future
-  // data.useDocker = !!process.env.USE_DOCKER;
-
   return {
     apiHost,
     enableSSO,
