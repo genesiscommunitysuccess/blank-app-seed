17c29c5e91eea459e9f2a121b40c34c8c5ee3d7f
Artur Cistov <artchistov@gmail.com>
2023-09-07 13:09:08 +0100

    feat: switch to declarative renaming, template rendering and answer data persistence PSD-11 (#87)

--- .genx/configure.js
+++ .genx/configure.js
@@ -1,74 +1,15 @@
-const { resolve } = require('node:path');
-const { listAllFiles, rename, writeJSON } = require('./utils');
 const versions = require('./versions.json');
 
 /**
  * Signature is `async (data: inquirer.Answers, utils: SeedConfigurationUtils)`
  */
 module.exports = async (data, utils) => {
-  const { writeFileWithData } = utils;
-  const { directory, appName, applicationVersion } = data;
-
   // populate additional data fields
-  data.pkgName = appName.replace(/[\W_]/g, '');
+  data.pkgName = data.appName.replace(/[\W_]/g, '');
   data.rootElement = `${data.pkgName}-root`;
-  data.localGenId = appName.toUpperCase().replace("-", "_");
-  data.applicationVersionWeb = applicationVersion.split('-').shift();
+  data.localGenId = data.appName.toUpperCase().replace("-", "_");
+  data.applicationVersionWeb = data.applicationVersion.split('-').shift();
   data.versions = versions;
   // to be exposed via user prompt in the future
   data.useDocker = !!process.env.USE_DOCKER;
-
-  // render files
-  const filesClient = [
-    `${directory}/README.md`,
-    `${directory}/client/package.json`,
-    `${directory}/client/index.html`,
-    `${directory}/client/src/main/main.ts`,
-    `${directory}/client/src/routes/config.ts`,
-    `${directory}/client/src/utils/logger.ts`,
-    `${directory}/client/public/initSSO.js`,
-    `${directory}/settings.gradle.kts`,
-    `${directory}/docker-compose.yml`
-  ];
-  const serverJvmRoot = resolve(directory, "server", "jvm");
-  const ignore = ['.DS_Store', '.gradle', '.lock', '.jar'];
-  const fileFilter = (item) => ignore.every((el) => !item.path.endsWith(el));
-  const filesServer = listAllFiles(serverJvmRoot, fileFilter)
-    .filter(item => !item.isDir)
-    .map(item => item.path);
-
-  [...filesClient, ...filesServer].forEach(item => writeFileWithData(item, data));
-
-  // rename files & folders
-  const placeholder = "genesis";
-  const dictionaryConfigFolder = resolve(serverJvmRoot, "genesis-config", "src", "main", "resources", "cfg");
-  const scriptConfigFolder = resolve(serverJvmRoot, "genesis-script-config", "src", "main", "resources", "scripts");
-  const dictionaryCacheRoot = resolve(directory, "server", "jvm", "genesis-dictionary-cache");
-  [
-    { item: "genesis-fields-dictionary.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-tables-dictionary.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-view-dictionary.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-system-definition.kts", dir: dictionaryConfigFolder },
-    { item: "genesis-processes.xml", dir: dictionaryConfigFolder },
-    { item: "genesis-service-definitions.xml", dir: dictionaryConfigFolder },
-    { item: "genesis-dataserver.kts", dir: scriptConfigFolder },
-    { item: "genesis-eventhandler.kts", dir: scriptConfigFolder },
-    { item: "genesis-reqrep.kts", dir: scriptConfigFolder },
-    { item: "genesis-generated-dao", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-fields", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-hft", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-sysdef", dir: dictionaryCacheRoot },
-    { item: "genesis-generated-view", dir: dictionaryCacheRoot },
-    { item: "genesis-dictionary-cache", dir: serverJvmRoot },
-    { item: "genesis-config", dir: serverJvmRoot },
-    { item: "genesis-deploy", dir: serverJvmRoot },
-    { item: "genesis-distribution", dir: serverJvmRoot },
-    { item: "genesis-eventhandler", dir: serverJvmRoot },
-    { item: "genesis-messages", dir: serverJvmRoot },
-    { item: "genesis-script-config", dir: serverJvmRoot },
-    { item: "genesis-site-specific", dir: serverJvmRoot },
-  ].forEach(el => rename(el.item, el.item.replace(placeholder, appName), el.dir));
-
-  // persist answers
-  writeJSON(`${__dirname}/answers.json`, data);
 };
