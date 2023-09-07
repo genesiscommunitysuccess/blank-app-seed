975ab1dadcdcc800002bf692716852c87341ea14
Artur Cistov <artur.cistov@genesis.global>
2023-09-07 09:24:25 +0100

    chore: wip PSD-11

--- .genx/.genxrc.js
+++ .genx/.genxrc.js
@@ -3,7 +3,7 @@ module.exports = {
   // Script used to configure the seed
   configure: () => require('./configure'),
   // Additional seed details beyond what was captured during seed registration
-  details: () => require('./package.json'),
+  details: () => require('./details'),
   // Prompts used to capture seed configuration values
   prompts: () => require('./prompts')
 };
