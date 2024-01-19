e2aa931c32224d0ee7869356a6dc4954a25e4623
Ali Al-Baity <ali.al-baity@genesis.global>
2024-01-19 08:38:37 +0000

    feat: Include settings plugin in blank app seed

--- server/jvm/{{appName}}-eventhandler/build.gradle.kts
+++ server/jvm/{{appName}}-eventhandler/build.gradle.kts
@@ -1,11 +1,11 @@
 dependencies {
     implementation(genesis("genesis-pal-execution"))
     implementation(genesis("genesis-eventhandler"))
-    implementation(project(":position-app-legacy-messages"))
-    api("global.genesis:genesis-db")
-    compileOnly(project(":position-app-legacy-config"))
-    testImplementation("global.genesis:genesis-dbtest")
-    testImplementation("global.genesis:genesis-testsupport")
+    implementation(project(":{{appName}}-messages"))
+    api(genesis("genesis-db"))
+    compileOnly(project(":{{appName}}-config"))
+    testImplementation(genesis("genesis-dbtest"))
+    testImplementation(genesis("genesis-testsupport"))
     genesisGeneratedCode()
 }
 
