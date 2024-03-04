11edb9263f62a938d5d801ee4211446c7dd5d36f
Karim Houari <karim.houari@genesis.global>
2024-03-04 10:00:10 +0000

    plugin

--- server/settings.gradle.kts
+++ server/settings.gradle.kts
@@ -13,32 +13,15 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven {
-            val repoUrl = if(properties["clientSpecific"] == "true") {
+            val repoUrl = if(extra.properties["clientSpecific"] == "true") {
                 "https://genesisglobal.jfrog.io/genesisglobal/libs-release-client"
             } else {
                 "https://genesisglobal.jfrog.io/genesisglobal/dev-repo"
             }
             url = uri(repoUrl)
             credentials {
-                username = properties["genesisArtifactoryUser"].toString()
-                password = properties["genesisArtifactoryPassword"].toString()
-            }
-            content {
-                fun disableIfTrue(
-                    property: String,
-                    moduleRegex: String,
-                ) {
-                    if (extra.properties[property] == "true") excludeModuleByRegex("global.genesis", moduleRegex)
-                }
-
-                disableIfTrue("localGenesis", "genesis-(?!crowley)[\\w-]+")
-                disableIfTrue("localAuth", "auth-[\\w-]+")
-                disableIfTrue("localFix", "fix-[\\w-]+")
-                disableIfTrue("localMarketData", "market-data-[\\w-]+")
-                disableIfTrue("localElektron", "elektron-[\\w-]+")
-                disableIfTrue("localRefData", "ref_data_app-[\\w-]+")
-                disableIfTrue("localDeployPlugin", "deploy-gradle-plugin")
-                disableIfTrue("localCrowley", "genesis-crowley-[\\w-]+")
+                username = extra.properties["genesisArtifactoryUser"].toString()
+                password = extra.properties["genesisArtifactoryPassword"].toString()
             }
         }
         mavenLocal {
