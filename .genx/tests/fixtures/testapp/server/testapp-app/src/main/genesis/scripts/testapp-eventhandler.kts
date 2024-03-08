f3e17a133975be9bfe321386239957965f154468
Karim Houari <karim.houari@genesis.global>
2024-03-08 16:28:24 +0000

    chore: update testapp folders

---
+++ .genx/tests/fixtures/testapp/server/testapp-app/src/main/genesis/scripts/testapp-eventhandler.kts
@@ -0,0 +1,21 @@
+/**
+ * System              : Genesis Business Library
+ * Sub-System          : multi-pro-code-test Configuration
+ * Version             : 1.0
+ * Copyright           : (c) Genesis
+ * Date                : 2022-03-18
+ * Function : Provide event handler config for multi-pro-code-test.
+ *
+ * Modification History
+ */
+eventHandler {
+
+    eventHandler<Trade>(name = "TRADE_INSERT") {
+        schemaValidation = false
+        onCommit { event ->
+            entityDb.insert(event.details)
+            ack()
+        }
+    }
+
+}
