2a1f927cb459e0ccafdbbbfff78ea2135c4ddaf7
Michal Skawianczyk <michal.skawianczyk@genesis.global>
2024-03-11 23:14:05 +0100

    feat: add ui pbc compatibility PLAT-1028 (#139)
    feat: add ui pbc compat PLAT-1028

--- client/src/routes/config.ts
+++ client/src/routes/config.ts
@@ -1,18 +1,12 @@
-import {
-  Auth,
-  FoundationAnalytics,
-  FoundationAnalyticsEvent,
-  FoundationAnalyticsEventType,
-  Session,
-} from '@genesislcap/foundation-comms';
+import { Auth, Session } from '@genesislcap/foundation-comms';
 import {
   defaultLoginConfig,
   LoginConfig,
   Settings as LoginSettings,
 } from '@genesislcap/foundation-login';
-import { Constructable } from '@microsoft/fast-element';
-import { Container, optional } from '@microsoft/fast-foundation';
-import { Route, RouterConfiguration } from '@microsoft/fast-router';
+import { FoundationRouterConfiguration } from '@genesislcap/foundation-ui';
+import { optional } from '@microsoft/fast-foundation';
+import { Route } from '@microsoft/fast-router';
 import { defaultLayout, loginLayout } from '../layouts';
 import { Home } from './home/home';
 import { NotFound } from './not-found/not-found';
@@ -31,11 +25,9 @@ const ssoSettings =
       }
     : {};
 
-export class MainRouterConfig extends RouterConfiguration<LoginSettings> {
+export class MainRouterConfig extends FoundationRouterConfiguration<LoginSettings> {
   constructor(
     @Auth private auth: Auth,
-    @Container private container: Container,
-    @FoundationAnalytics private analytics: FoundationAnalytics,
     @Session private session: Session,
     @optional(LoginConfig)
     private loginConfig: LoginConfig = { ...defaultLoginConfig, autoAuth: true, autoConnect: true },
@@ -43,9 +35,8 @@ export class MainRouterConfig extends RouterConfiguration<LoginSettings> {
     super();
   }
 
-  public allRoutes = [{ index: 1, path: 'home', title: 'Home', icon: 'home', variant: 'solid' }];
-
-  public configure() {
+  async configure() {
+    this.configureAnalytics();
     this.title = 'Blank App Demo';
     this.defaultLayout = defaultLayout;
 
@@ -78,11 +69,23 @@ export class MainRouterConfig extends RouterConfiguration<LoginSettings> {
         settings: { public: true },
         childRouters: true,
       },
-      { path: 'home', element: Home, title: 'Home', name: 'home' },
+      {
+        path: 'home',
+        name: 'home',
+        title: 'Home',
+        element: Home,
+        navItems: [
+          {
+            title: 'Home',
+            icon: {
+              name: 'home',
+              variant: 'solid',
+            },
+          },
+        ],
+      },
       { path: 'not-found', element: NotFound, title: 'Not Found', name: 'not-found' },
     );
-
-    const auth = this.auth;
 
     /**
      * Example of a FallbackRouteDefinition
@@ -97,12 +100,6 @@ export class MainRouterConfig extends RouterConfiguration<LoginSettings> {
     this.contributors.push({
       navigate: async (phase) => {
         const settings = phase.route.settings;
-
-        this.analytics.trackEvent(FoundationAnalyticsEventType.routeChanged, <
-          FoundationAnalyticsEvent.RouteChanged
-        >{
-          path: phase.route.endpoint.path,
-        });
 
         /**
          * If public route don't block
@@ -121,7 +118,7 @@ export class MainRouterConfig extends RouterConfiguration<LoginSettings> {
         /**
          * If allowAutoAuth and session is valid try to connect+auto-login
          */
-        if (this.loginConfig.autoAuth && (await auth.reAuthFromSession())) {
+        if (this.loginConfig.autoAuth && (await this.auth.reAuthFromSession())) {
           return;
         }
 
@@ -134,9 +131,5 @@ export class MainRouterConfig extends RouterConfiguration<LoginSettings> {
         });
       },
     });
-  }
-
-  public construct<T>(Type: Constructable<T>): T {
-    return this.container.get(Type) as T;
   }
 }
