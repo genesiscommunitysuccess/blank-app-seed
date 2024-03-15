5f4d3ce0a668864744e1a014bfae57e69776cf61
michal.skawianczyk <michal.skawianczyk@genesis.global>
2024-03-15 16:56:28 +0100

    feat: component generation GENC-57

--- .genx/prompts/ui.js
+++ .genx/prompts/ui.js
@@ -2,74 +2,6 @@ const routesInto = () => console.log(`
   Pages to be added to the navigation header
 `);
 
-const routes2 = [
-  {
-    name: "home",
-    tiles: [
-      {
-        title: "Entity manager tile",
-        type: "entityManager",
-        config: {
-          resourceName: "ALL_COUNTERPARTYS",
-          title: "Counterparty Management",
-          updateEvent: "EVENT_COUNTERPARTY_MODIFY",
-          deleteEvent: "EVENT_COUNTERPARTY_DELETE",
-          createEvent: "EVENT_COUNTERPARTY_INSERT"
-        }
-      },
-      {
-        title: "Form tile",
-        type: "form",
-        config: {
-          resourceName: "EVENT_TRADE_INSERT"
-        }
-      }
-    ]
-  },
-  {
-    name: "page-2",
-    tiles: [
-      {
-        title: "Grid Tile",
-        type: "grid",
-        config: {
-          resourceName: "ALL_POSITIONS"
-        }
-      },
-      {
-        title: "Charts Tile 1",
-        type: "chart",
-        config: {
-          type: "line",
-          resourceName: "ALL_POSITIONS",
-          xField: 'INSTRUMENT_NAME',
-          yField: 'VALUE'
-        }
-      },
-      {
-        title: "Charts Tile 2",
-        type: "chart",
-        config: {
-          type: "pie",
-          resourceName: "ALL_POSITIONS",
-          xField: 'INSTRUMENT_NAME',
-          yField: 'VALUE'
-        },
-      },
-      {
-        title: "Charts Tile 3",
-        type: "chart",
-        config: {
-          type: "column",
-          resourceName: "ALL_POSITIONS",
-          xField: 'INSTRUMENT_ID',
-          yField: 'VALUE'
-        }
-      }
-    ]
-  }
-]
-
 module.exports = async (inquirer, prevAns = {}) => {
   routesInto();
   const {
@@ -80,11 +12,21 @@ module.exports = async (inquirer, prevAns = {}) => {
       type: 'input',
       message: 'Pages in comma separated format e.g. home,dashboard,admin',
       when: !prevAns.routes,
-      default: prevAns.routes || 'home',
+      default: `[{name: 'home'}]`,
     },
   ])
 
+  let routesParsed = [{name: 'home'}];
+  if (routes) {
+    try {
+      routesParsed = JSON.parse(routes);
+      console.log(routesParsed)
+    } catch (error) {
+      console.error("Error parsing JSON, will use default routes config:", error);
+    }
+  }
+
   return {
-    routes: routes2,
+    routes: routesParsed,
   };
 };
