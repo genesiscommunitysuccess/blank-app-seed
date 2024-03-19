9933d523639827d849962e45278c25bffdc3dafe
Artur Cistov <artur.cistov@genesis.global>
2024-03-19 11:24:03 +0000

    fix: make absolute app paths work with local seeds GENC-183

--- .genx/configure.js
+++ .genx/configure.js
@@ -1,13 +1,6 @@
 const versions = require('./versions.json');
-const path = require('path');
-const fs = require('fs');
-const directoryExists = (directory) => fs.existsSync(directory);
+const { registerPartials, generateRoute } = require('./utils');
 
-const makeDirectory = (directory) => {
-  if (!directoryExists(directory)) {
-    fs.mkdirSync(directory);
-  }
-};
 /**
  * Signature is `async (data: inquirer.Answers, utils: SeedConfigurationUtils)`
  */
@@ -19,18 +12,10 @@ module.exports = async (data, utils) => {
   data.applicationVersionWeb = data.applicationVersion.split('-').shift();
   data.versions = versions;
 
-  utils.registerPartial('smart-form', path.resolve(data.directory, '.genx/templates/form.hbs'))
-  utils.registerPartial('chart', path.resolve(data.directory, '.genx/templates/chart.hbs'))
-  utils.registerPartial('entity-manager', path.resolve(data.directory, '.genx/templates/entityManager.hbs'))
-  utils.registerPartial('grid-pro', path.resolve(data.directory, '.genx/templates/grid.hbs'))
-  // to be exposed via user prompt in the future
-  data.useDocker = !!process.env.USE_DOCKER;
+  registerPartials(utils);
+
   data.routes.forEach(route => {
-    const routeName = utils.changeCase.paramCase(route.name);
-    makeDirectory(path.resolve(data.directory,`client/src/routes/${routeName}`));
-    utils.writeFileWithData(path.resolve(data.directory, `client/src/routes/${routeName}/${routeName}.ts`), {route}, path.resolve(data.directory, '.genx/templates/route.hbs'));
-    utils.writeFileWithData(path.resolve(data.directory, `client/src/routes/${routeName}/${routeName}.template.ts`), {route}, path.resolve(data.directory, '.genx/templates/route.template.hbs'));
-    utils.writeFileWithData(path.resolve(data.directory, `client/src/routes/${routeName}/${routeName}.styles.ts`), {route}, path.resolve(data.directory, '.genx/templates/route.styles.hbs'));
-  })
+    generateRoute(route, utils);
+  });
 
 };
---
+++ .genx/utils.js
@@ -0,0 +1,29 @@
+const {existsSync, mkdirSync} = require('node:fs');
+const { resolve } = require('node:path');
+
+const makeDirectory = (directory) => {
+  if (!existsSync(directory)) {
+    mkdirSync(directory);
+  }
+};
+
+const registerPartials = ({ registerPartial }) => {
+  registerPartial('smart-form', resolve(__dirname, 'templates/form.hbs'))
+  registerPartial('chart', resolve(__dirname, 'templates/chart.hbs'))
+  registerPartial('entity-manager', resolve(__dirname, 'templates/entityManager.hbs'))
+  registerPartial('grid-pro', resolve(__dirname, 'templates/grid.hbs'))
+};
+
+const generateRoute = (route, { writeFileWithData, changeCase }) => {
+  const routeName = changeCase.paramCase(route.name);
+  makeDirectory(resolve(data.directory, `client/src/routes/${routeName}`));
+  writeFileWithData(resolve(__dirname, `../client/src/routes/${routeName}/${routeName}.ts`), {route}, resolve(__dirname, 'templates/route.hbs'));
+  writeFileWithData(resolve(__dirname, `../client/src/routes/${routeName}/${routeName}.template.ts`), {route}, resolve(__dirname, 'templates/route.template.hbs'));
+  writeFileWithData(resolve(__dirname, `../client/src/routes/${routeName}/${routeName}.styles.ts`), {route}, resolve(__dirname, 'templates/route.styles.hbs'));
+};
+
+module.exports = {
+  makeDirectory,
+  registerPartials,
+  generateRoute
+};
