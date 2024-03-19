816987f8fd75495ce239874422e0925a767ceed5
Artur Cistov <artur.cistov@genesis.global>
2024-03-19 11:35:29 +0000

    chore: wip GENC-183

--- .genx/utils.js
+++ .genx/utils.js
@@ -16,7 +16,7 @@ const registerPartials = ({ registerPartial }) => {
 
 const generateRoute = (route, { writeFileWithData, changeCase }) => {
   const routeName = changeCase.paramCase(route.name);
-  makeDirectory(resolve(data.directory, `client/src/routes/${routeName}`));
+  makeDirectory(resolve(__dirname, `../client/src/routes/${routeName}`));
   writeFileWithData(resolve(__dirname, `../client/src/routes/${routeName}/${routeName}.ts`), {route}, resolve(__dirname, 'templates/route.hbs'));
   writeFileWithData(resolve(__dirname, `../client/src/routes/${routeName}/${routeName}.template.ts`), {route}, resolve(__dirname, 'templates/route.template.hbs'));
   writeFileWithData(resolve(__dirname, `../client/src/routes/${routeName}/${routeName}.styles.ts`), {route}, resolve(__dirname, 'templates/route.styles.hbs'));
