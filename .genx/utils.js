a7b051d8bc1a8d77035fa007760f80b7b8953fef
Szymon Żur <szymon.zur2@gmail.com>
2024-04-12 18:24:43 +0200

    fix: build test error GENC-286

--- .genx/utils.js
+++ .genx/utils.js
@@ -67,9 +67,9 @@ const validateRoute = (route) => {
 }
 
 const getLayoutType = (route) => {
-  if (route?.tiles.length < 4) {
+  if (route?.tiles?.length < 4) {
     return 'horizontal-layout'
-  } else if (route?.tiles.length === 4) {
+  } else if (route?.tiles?.length === 4) {
     return 'grid-layout'
   } 
   return 'tabs-layout'
