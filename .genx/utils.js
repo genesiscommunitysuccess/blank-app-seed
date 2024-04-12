06a664155fa34c239fe234519c7bd720954cea01
Szymon Żur <szymon.zur2@gmail.com>
2024-04-12 18:54:33 +0200

    feat: new layout types GENC-286 (#182)
    * feat: new layout types GENC-286
    
    * feat: new layout types GENC-286
    
    * fix: build test error GENC-286

--- .genx/utils.js
+++ .genx/utils.js
@@ -8,6 +8,9 @@ const makeDirectory = (directory) => {
 };
 
 const registerPartials = ({ registerPartial }) => {
+  registerPartial('grid-layout', resolve(__dirname, 'templates/gridLayout.hbs'))
+  registerPartial('tabs-layout', resolve(__dirname, 'templates/tabsLayout.hbs'))
+  registerPartial('horizontal-layout', resolve(__dirname, 'templates/horizontalLayout.hbs'))
   registerPartial('smart-form', resolve(__dirname, 'templates/form.hbs'))
   registerPartial('chart', resolve(__dirname, 'templates/chart.hbs'))
   registerPartial('entity-manager', resolve(__dirname, 'templates/entityManager.hbs'))
@@ -63,8 +66,18 @@ const validateRoute = (route) => {
   return !!route.name;
 }
 
+const getLayoutType = (route) => {
+  if (route?.tiles?.length < 4) {
+    return 'horizontal-layout'
+  } else if (route?.tiles?.length === 4) {
+    return 'grid-layout'
+  } 
+  return 'tabs-layout'
+}
+
 const formatRouteData = (route) => {
   const layoutKey = route?.layoutKey || `${route.name}_${Date.now()}`;
+  const layoutType = route?.layoutType || getLayoutType(route);
 
   const FDC3ClickCategory = 'fdc3';
   const FDC3EventHandlersEnabled = !!route.tiles?.find(t => t.config?.gridOptions?.onRowClicked?.category === FDC3ClickCategory);
@@ -83,6 +96,7 @@ const formatRouteData = (route) => {
 
   return {
     ...route,
+    layoutType,
     layoutKey,
     tiles,
     FDC3EventHandlersEnabled,
