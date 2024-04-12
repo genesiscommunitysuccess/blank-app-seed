645723acb0ec3d85ff3ef59c0d05104d10e349c8
Szymon Żur <szymon.zur2@gmail.com>
2024-04-12 18:14:32 +0200

    feat: new layout types GENC-286

--- .genx/utils.js
+++ .genx/utils.js
@@ -66,8 +66,18 @@ const validateRoute = (route) => {
   return !!route.name;
 }
 
+const getLayoutType = (route) => {
+  if (route?.tiles.length < 4) {
+    return 'horizontal-layout'
+  } else if (route?.tiles.length === 4) {
+    return 'grid-layout'
+  } 
+  return 'tabs-layout'
+}
+
 const formatRouteData = (route) => {
   const layoutKey = route?.layoutKey || `${route.name}_${Date.now()}`;
+  const layoutType = route?.layoutType || getLayoutType(route);
 
   const FDC3ClickCategory = 'fdc3';
   const FDC3EventHandlersEnabled = !!route.tiles?.find(t => t.config?.gridOptions?.onRowClicked?.category === FDC3ClickCategory);
@@ -86,6 +96,7 @@ const formatRouteData = (route) => {
 
   return {
     ...route,
+    layoutType,
     layoutKey,
     tiles,
     FDC3EventHandlersEnabled,
