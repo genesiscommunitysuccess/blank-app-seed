efeda8c90dc05e72632eca439980d2a86f7d9f43
Szymon Żur <szymon.zur2@gmail.com>
2024-04-12 17:00:13 +0200

    feat: new layout types GENC-286

--- .genx/utils.js
+++ .genx/utils.js
@@ -8,6 +8,9 @@ const makeDirectory = (directory) => {
 };
 
 const registerPartials = ({ registerPartial }) => {
+  registerPartial('grid-layout', resolve(__dirname, 'templates/gridLayout.hbs'))
+  registerPartial('tabs-layout', resolve(__dirname, 'templates/tabsLayout.hbs'))
+  registerPartial('horizontal-layout', resolve(__dirname, 'templates/horizontalLayout.hbs'))
   registerPartial('smart-form', resolve(__dirname, 'templates/form.hbs'))
   registerPartial('chart', resolve(__dirname, 'templates/chart.hbs'))
   registerPartial('entity-manager', resolve(__dirname, 'templates/entityManager.hbs'))
