f23d2321cd58f05a09fc04eff2f95574c2587bc2
Szymon Żur <szymon.zur2@gmail.com>
2024-04-23 01:27:05 +0200

    fix: lint error GENC-265

--- client/src/utils/formatters.ts
+++ client/src/utils/formatters.ts
@@ -2,14 +2,13 @@ import Numeral from 'numeral';
 import 'numeral/locales';
 
 export function getNumberFormatter(format: string, locale?: string) {
-    return (params) => {
-      if (!(params && typeof params.value === 'number')) return '';
-  
-      if (locale) {
-        Numeral.locale(locale);
-      }
-  
-      return Numeral(params.value).format(format);
-    };
-  }
-  
+  return (params) => {
+    if (!(params && typeof params.value === 'number')) return '';
+
+    if (locale) {
+      Numeral.locale(locale);
+    }
+
+    return Numeral(params.value).format(format);
+  };
+}
