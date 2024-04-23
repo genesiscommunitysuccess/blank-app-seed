b1cfe817a53d20033660b93305a2a3c210152799
Szymon Żur <szymon.zur2@gmail.com>
2024-04-23 16:05:33 +0200

    chore: pass options config to dateFormatter GENC-318

--- client/src/utils/formatters.ts
+++ client/src/utils/formatters.ts
@@ -1,6 +1,13 @@
 import Numeral from 'numeral';
 import 'numeral/locales';
 
+const defaultDateOptions: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  timeZone: 'UTC',
+};
+
 export function getNumberFormatter(format: string, locale?: string) {
   return (params) => {
     if (!(params && typeof params.value === 'number')) return '';
@@ -13,17 +20,13 @@ export function getNumberFormatter(format: string, locale?: string) {
   };
 }
 
-export function getDateFormatter(locale: string = 'en-GB', withTime: boolean) {
+export function getDateFormatter(
+  locale: string = 'en-GB',
+  options: Intl.DateTimeFormatOptions = defaultDateOptions,
+) {
   return (params) => {
     if (!(params && typeof params.value === 'number')) return '';
 
-   return  new Intl.DateTimeFormat(locale, {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      timeZone: 'UTC',
-      ...(withTime ? {hour: '2-digit',
-      minute: '2-digit'} : {})
-    }).format(params.value);
-  }
+    return new Intl.DateTimeFormat(locale, options).format(params.value);
+  };
 }
