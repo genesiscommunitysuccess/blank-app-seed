d58cf9372b13c258a9f2f388581ac78c4668383d
Szymon Żur <szymon.zur2@gmail.com>
2024-04-23 15:28:13 +0200

    feat: add getDateFormatter GENC-318

--- client/src/utils/formatters.ts
+++ client/src/utils/formatters.ts
@@ -12,3 +12,18 @@ export function getNumberFormatter(format: string, locale?: string) {
     return Numeral(params.value).format(format);
   };
 }
+
+export function getDateFormatter(locale: string = 'en-GB', withTime: boolean) {
+  return (params) => {
+    if (!(params && typeof params.value === 'number')) return '';
+
+   return  new Intl.DateTimeFormat(locale, {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      timeZone: 'UTC',
+      ...(withTime ? {hour: '2-digit',
+      minute: '2-digit'} : {})
+    }).format(params.value);
+  }
+}
