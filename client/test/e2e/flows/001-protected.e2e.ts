9e469a6d93ca3be77c71b973fae19cc30ba0d938
Artur Cistov <artur.cistov@genesis.global>
2024-04-29 10:01:00 +0100

    chore: wip GENC-375

--- client/test/e2e/flows/001-protected.e2e.ts
+++ client/test/e2e/flows/001-protected.e2e.ts
@@ -2,5 +2,5 @@ import { expect } from '@genesislcap/foundation-testing/e2e';
 import { test } from '../fixture';
 
 test('expected page title', async ({ protectedPage, page }) => {
-  await expect(page).toHaveTitle(/Blank App Demo - Login - Login/);
+  await expect(page).toHaveTitle(/Testapp - Login - Login/);
 });
