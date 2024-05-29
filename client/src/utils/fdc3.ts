a6060111a29dc4a8104ea43fabe7faa9958de2c7
Matt Walker <43502076+matteematt@users.noreply.github.com>
2024-05-29 11:04:41 +0000

    Revert "fix: adding foundation-fdc3 - GENC-292 (#239)"
    This reverts commit fb1676d5b9a9fc9e12282cc4e9e7ef3148e172a7.

--- client/src/utils/fdc3.ts
+++ client/src/utils/fdc3.ts
@@ -1,17 +1,30 @@
 {{#if FDC3.includeDependencies}}
-import { DefaultFDC3 } from '@genesislcap/foundation-fdc3';
-
+import { getOrCreateChannel, raiseIntent, addIntentListener } from '@finos/fdc3';
 {{/if}}
 export const isFDC3 = (): boolean => !!(window as any).fdc3;
 {{#if FDC3.includeDependencies}}
-const fdc3Service = new DefaultFDC3();
 
 export const onFDC3Ready = async (FDC3ReadyCb: () => any): Promise<void> => {
   isFDC3()
     ? await FDC3ReadyCb()
     : window.addEventListener('fdc3Ready', async () => {
-      await FDC3ReadyCb();
-    });
+        await FDC3ReadyCb();
+      });
+};
+
+export const sendMessageOnChannel = async (
+  channelName: string,
+  type: string,
+  payload: any,
+): Promise<void> => {
+  const channel = await getOrCreateChannel(channelName);
+
+  const m: any = {
+    type,
+    id: payload,
+  };
+
+  await channel.broadcast(m);
 };
 
 export const listenToChannel = async (
@@ -19,14 +32,47 @@ export const listenToChannel = async (
   type: string,
   callback: (result: any) => void,
 ): Promise<void> => {
-  fdc3Service.addChannelListener(channelName, type, callback);
+  const channel = await getOrCreateChannel(channelName);
+  channel.addContextListener(type, (result) => callback(result));
 };
+
+export const doRaiseIntent = async (intent: string, type: string, context: any): Promise<void> => {
+  const message = {
+    type,
+    id: context,
+  };
+
+  const result = await raiseIntent(intent, message);
+};
+
+export const listenForIntent = async (
+  intent: string,
+  callback: (result: any) => void,
+): Promise<void> => {
+  addIntentListener(intent, (result) => callback(result));
+};
+
+export const stripOutBigInt = (object: any): any => {
+
+  object = { ...object };
+
+  Object.keys(object).forEach(key => {
+    if (typeof object[key] === 'bigint') {
+      delete object[key]
+    } else if (typeof object[key] === 'object') {
+      object[key] = stripOutBigInt(object[key]);
+    }
+  })
+
+  return object;
+}
 
 export const sendEventOnChannel = (channelName: string, type: string) => {
   return async (e: any) => {
     // check for ag-grid-specific events, fall back to standard events
     const payload = e.data || e.detail;
-    await fdc3Service.broadcastOnChannel(channelName, type, payload);
+    const sanitised = stripOutBigInt(payload);
+    sendMessageOnChannel(channelName, type, sanitised);
   };
 };
 {{/if}}
