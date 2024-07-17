d83b4799aeab9e2c2a4cd29f9f3f296543fdfef2
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-07-17 13:26:17 +0200

    feat: fix fdc3 - FUI-2068

--- client-tmp/angular/src/app/utils/fdc3.ts
+++ client-tmp/angular/src/app/utils/fdc3.ts
@@ -3,7 +3,6 @@ import { DefaultFDC3 } from '@genesislcap/foundation-fdc3';
 {{/if}}
 export const isFDC3 = (): boolean => !!(window as any).fdc3;
 {{#if FDC3.includeDependencies}}
-const fdc3Service = new DefaultFDC3();
 
 export const onFDC3Ready = async (FDC3ReadyCb: () => any): Promise<void> => {
   isFDC3()
@@ -18,11 +17,13 @@ export const listenToChannel = async (
   type: string,
   callback: (result: any) => void,
 ): Promise<void> => {
+  const fdc3Service = new DefaultFDC3();
   fdc3Service.addChannelListener(channelName, type, callback);
 };
 
 export const sendEventOnChannel = (channelName: string, type: string) => {
   return async (e: any) => {
+    const fdc3Service = new DefaultFDC3();
     // check for ag-grid-specific events, fall back to standard events
     const payload = e.data || e.detail;
     await fdc3Service.broadcastOnChannel(channelName, type, payload);
