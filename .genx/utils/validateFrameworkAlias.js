6d615023c956511a00ada08f3ca1ac70e67b34fc
Artur Krasiński <artur@minion.pl>
2024-07-25 11:20:35 +0000

    feat: Implement the generated UI code structure proposal (angular) (#287)
    * chore: change generated UI code structure - FUI-2099
    
    * chore: change generated UI code structure - FUI-2099
    
    * chore: mplement the generated UI code structure proposal - FUI-2099

--- .genx/utils/validateFrameworkAlias.js
+++ .genx/utils/validateFrameworkAlias.js
@@ -1,7 +1,12 @@
 const { FRAMEWORKS_ALIAS } = require('../static');
 const normalizeFrameworkAlias = require('./normalizeFrameworkAlias');
+const isString = require('./isString');
 
 const validateFrameworkAlias = (alias) => {
+  if (!isString(alias)) {
+    return false;
+  }
+
   return FRAMEWORKS_ALIAS.includes(normalizeFrameworkAlias(alias));
 };
 
