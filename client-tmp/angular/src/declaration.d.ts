3f12e7a13aeec43adc431c80f59aa998a2aefafa
derekdon <derekdon@protonmail.com>
2024-07-26 09:30:25 +0100

    feat: update declaration file FUI-2065

--- client-tmp/angular/src/declaration.d.ts
+++ client-tmp/angular/src/declaration.d.ts
@@ -2,4 +2,10 @@ declare module "*.svg" {
   const content: any;
   export default content;
 }
-  
+
+/**
+ * Temp to unblock testing until we update the types pointer in all PBC packages
+ */
+declare module '@genesislcap/pbc-auth-ui';
+declare module '@genesislcap/pbc-documents-ui';
+declare module '@genesislcap/pbc-reconciliation-ui';
