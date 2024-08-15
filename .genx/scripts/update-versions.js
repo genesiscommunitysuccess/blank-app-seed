be694bd315e52407e41d0527f98d75496c260230
Rafael Ferreira <rafael.ferreira@genesis.global>
2024-08-15 10:41:32 +0100

    chore: updating scripts to allow skipServer in prerelease (GENC-704)

--- .genx/scripts/update-versions.js
+++ .genx/scripts/update-versions.js
@@ -4,23 +4,30 @@ const { resolve } = require('node:path');
 const current = require('../versions.json');
 
 const args = process.argv.slice(2);
-const patchOnly = args.indexOf("--patch-only") >= 0;
-const dryRun = args.indexOf("--dry-run") >= 0;
+
+const argDefined = (name) => args.indexOf(name) >= 0;
+
+const patchOnly = argDefined("--patch-only");
+const dryRun = argDefined("--dry-run");
+const skipServer = argDefined("--skip-server");
 
 const VERSION_REGEX = /\d+$/;
+const JFROG_BASE_EXCLUSIONS = '*-SNAPSHOT*;*maven-metadata*;*test*;*TEST*';
 
-const jfrogVersionMatcher = patchOnly ? current.GSF.replace(VERSION_REGEX, '*') : undefined;
+const { jfrogVersionMatcher, jfrogExclusionsMatcher, npmVersionMatcher } = patchOnly
+  ? { jfrogVersionMatcher: current.GSF.replace(VERSION_REGEX, '*'), jfrogExclusionsMatcher: `*-beta*;*-RC*;${JFROG_BASE_EXCLUSIONS}`, npmVersionMatcher: current.UI.replace(VERSION_REGEX, 'x') }
+  : { jfrogVersionMatcher: undefined, jfrogExclusionsMatcher: JFROG_BASE_EXCLUSIONS, npmVersionMatcher: 'latest' };
 
-const npmVersionMatcher = patchOnly ? current.UI.replace(VERSION_REGEX, 'x') : 'latest';
+console.log('Running with: ', { patchOnly, skipServer, dryRun, npmVersionMatcher, jfrogVersionMatcher, jfrogExclusionsMatcher });
 
-console.log('Running with: ', {patchOnly, dryRun, npmVersionMatcher, jfrogVersionMatcher})
-
-const run = (command) =>
-  execSync(command, {
+const run = (command) => {
+  console.debug('running:', command);
+  return execSync(command, {
     stdio: ['pipe', 'pipe', 'ignore'],
   })
     .toString('utf8')
     .trim();
+};
 
 const writeJSON = (json, path) => {
   const data = JSON.stringify(json, null, 2) + '\n';
@@ -34,13 +41,19 @@ const writeJSON = (json, path) => {
 
 const UI = run(`npm info @genesislcap/foundation-ui@${npmVersionMatcher} version`);
 
-const GSF = run(
-  `jf rt s "libs-release-client/global/genesis/genesis-distribution/${jfrogVersionMatcher ?? ''}" --exclusions="*-RC*;*-SNAPSHOT*;*maven-metadata*;*test*;*TEST*" | grep path | tr -s ' ' | sed 's/"path": //g' | awk -F'/' '{print $(NF-1)}' | sort -V | tail -n 1`,
-);
+const serverOsCommandPipeline = `grep path | tr -s ' ' | sed 's/"path": //g' | awk -F'/' '{print $(NF-1)}' | sort -V | tail -n 1`;
 
-const Auth = run(
-  `jf rt s "libs-release-client/global/genesis/auth-distribution/${jfrogVersionMatcher ?? ''}" --exclusions="*-RC*;*-SNAPSHOT*;*maven-metadata*;*test*;*TEST*" | grep path | tr -s ' ' | sed 's/"path": //g' | awk -F'/' '{print $(NF-1)}' | sort -V | tail -n 1`,
-);
+const { GSF, Auth } = skipServer
+  ? { GSF: current.GSF, Auth: current.Auth }
+  : {
+    GSF: run(
+      `jf rt s "libs-release-client/global/genesis/genesis-distribution/${jfrogVersionMatcher ?? ''}" --exclusions="${jfrogExclusionsMatcher}" | ${serverOsCommandPipeline}`
+    ),
+    Auth: run(
+      `jf rt s "libs-release-client/global/genesis/auth-distribution/${jfrogVersionMatcher ?? ''}" --exclusions="${jfrogExclusionsMatcher}" | ${serverOsCommandPipeline}`,
+    )
+  };
+
 const latest = { UI, GSF, Auth };
 
 console.log('Current:', current);
