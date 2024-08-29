3367fda8ed335cbdae06e106b0231b3fb88916af
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-08-29 12:08:34 +0200

    chore:  wip FUI-2153

--- client-tmp/react/vite.config.ts
+++ client-tmp/react/vite.config.ts
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      'foundationZero/ZeroDesignSystem': resolve(__dirname, 'node_modules/@genesislcap/foundation-zero'),
+export default defineConfig(({ mode }) => {
+  const API_HOST = mode === 'development' ? '{{apiHost}}' : '';
+
+  return {
+    plugins: [
+      react(),
+    ],
+    define: {
+      'process.env.ENABLE_SSO': false,
+      'process.env.API_HOST': API_HOST,
     },
-  },
+    resolve: {
+      alias: {
+        'foundationZero/ZeroDesignSystem': resolve(__dirname, 'node_modules/@genesislcap/foundation-zero'),
+      },
+    },
+  }
 });
