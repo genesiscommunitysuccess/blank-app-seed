a6ca52d69cde09aab0666ec62e6d2691df823d71
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-09-03 11:58:22 +0200

    chore: support env variables - FUI-2153

--- client-tmp/react/src/utils/setApiHost.ts
+++ client-tmp/react/src/utils/setApiHost.ts
@@ -3,7 +3,7 @@ import { API_DATA } from '../config';
 export const setApiHost = () => {
     const { URL: apiHost } = API_DATA;
   
-    if (apiHost && sessionStorage.getItem('hostUrl') === null) {
+    if (apiHost) {
       sessionStorage.setItem('hostUrl', apiHost);
     }
   };
--- client-tmp/react/vite.config.js
+++ client-tmp/react/vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath } from 'node:url';
-import { resolve } from 'path';
-import path from 'node:path';
+import { resolve, dirname } from 'path';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 import react from '@vitejs/plugin-react';
 import visualizer from 'rollup-plugin-visualizer';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
   const https =  process.env.HTTPS === 'true';
