8d7256c8b430c385ddc5430dee136bfb63475ac0
Artur Krasiński <artur@minion.pl>
2024-09-03 17:33:29 +0200

    chore: support env variables - FUI-2153 (#330)
    * chore: support env variables - FUI-2153

--- client-tmp/react/vite.config.js
+++ client-tmp/react/vite.config.js
@@ -1,17 +1,29 @@
 import { fileURLToPath } from 'node:url';
-import { resolve } from 'path';
-import path from 'node:path';
+import { resolve, dirname } from 'path';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 import react from '@vitejs/plugin-react';
 import visualizer from 'rollup-plugin-visualizer';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
   const https =  process.env.HTTPS === 'true';
   const open = !(process.env.NO_OPEN === 'true');
+  const jsonFilePath = resolve(process.cwd(), `env.${mode}.json`);
+  const envConfig = {};
+  
+  if (fs.existsSync(jsonFilePath)) {
+    const jsonContent = fs.readFileSync(jsonFilePath, 'utf-8');
+    const parsedConfig = JSON.parse(jsonContent);
+  
+    for (const key in parsedConfig) {
+      envConfig[`import.meta.env.${key}`] = JSON.stringify(parsedConfig[key]);
+    }
+  }
 
   const config = {
+    define: envConfig,
     server: {
       https,
       open,
