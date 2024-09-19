c064a68cef3d6efcee66a9c3342e0ea6b60fd724
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-09-19 18:55:28 +0200

    feat: replaced Karma with Jest

--- client-tmp/angular/karma.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(config) {
-  config.set({
-    browsers: ['Chrome'],
-    browserDisconnectTimeout: 60000, 
-    browserDisconnectTolerance: 3, 
-    browserNoActivityTimeout: 60000, 
-    captureTimeout: 120000,
-  });
-};
