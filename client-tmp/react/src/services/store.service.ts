dcb7e0345e66a383c1180353dfb5598376029bca
ArturKrasinski <kontakt@arturkrasinski.pl>
2024-09-26 10:46:29 +0200

    feat: PBC support in react - FUI-2140

--- client-tmp/react/src/services/store.service.ts
+++ client-tmp/react/src/services/store.service.ts
@@ -1,11 +1,39 @@
 
-import { getStore as fetchStore } from '@/utils';
+import { CustomEventMap } from '@genesislcap/foundation-events';
+import { getApp } from '@genesislcap/foundation-shell/app';
+import {
+  AbstractStoreRoot,
+  registerStore,
+  StoreRoot,
+  StoreRootEventDetailMap,
+} from '@genesislcap/foundation-store';
+import { DI } from '@genesislcap/web-core';
+
+interface Store extends StoreRoot {}
+type StoreEventDetailMap = StoreRootEventDetailMap & {};
+
+declare global {
+  interface HTMLElementEventMap extends CustomEventMap<StoreEventDetailMap> {}
+}
+
+class DefaultStore extends AbstractStoreRoot<Store, StoreEventDetailMap> implements Store {
+  constructor() {
+    super();
+
+    /**
+     * Register the store root
+     */
+    getApp().registerStoreRoot(this);
+  }
+}
+
+const Store = registerStore(DefaultStore, 'Store');
 
 class StoreService {
   private store: any;
 
   constructor() {
-    this.store = fetchStore();
+    this.store = DI.getOrCreateDOMContainer().get(Store) as Store;
   }
 
   getStore() {
